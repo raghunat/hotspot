46085a648d8a39f721de19f5e5d5e8d9639e76a0
Stephen Raghunath <stephen.raghunath@acquia.com>
2017-03-07 12:50:58 -0500

    Removing bad commit conflict markerswq

--- users-api/api/alive.spec.js
+++ users-api/api/alive.spec.js
@@ -5,18 +5,10 @@ const supertest = require('supertest');
 describe('/alive', () => {
   it('should return an alive message', done => {
     supertest(app)
-<<<<<<< HEAD
-    .get('alive')
-    .end((err, res) => {
-      res.text.should.equal('Johnny Five is alive!');
-      done();
-    });
-=======
       .get('/alive')
       .end((err, res) => {
         res.text.should.equal('Johnny Five is Alive!');
         done();
       });
->>>>>>> a5d4a148386296b93963dbe96a899075d7fef326
   });
 });
