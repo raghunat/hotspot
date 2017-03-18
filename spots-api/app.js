998dc4cd00c296f707e9d615857d9ece4318cb06
Stephen Raghunath <raghunat@fredonia.edu>
2017-03-18 07:45:32 -0400

    Added mongo db connection

--- spots-api/app.js
+++ spots-api/app.js
@@ -1,6 +1,18 @@
+// app.js
+// Bootstrapping the express application
 const app = require('express')();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const path = require('path');
+
+// Load environment secret variables
+require('dotenv').config({
+  path: path.join(__dirname, '../.env')
+});
+
+// Connection URL
+const mongoURL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ds119210.mlab.com:19210/hotspotdb`;
 
 // Allow Cross origin requests
 app.use(cors());
@@ -9,11 +21,19 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-// Load in routes:
-app.get('/alive', require('./api/alive.js')());
-app.get('/spots', require('./api/get-spots.js')());
-app.post('/spots', require('./api/post-spots.js')());
-app.put('/spots/:id', require('./api/put-spot.js')());
+// Use connect method to connect to the server
+MongoClient.connect(mongoURL, function(err, db) {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected successfully to mongo");
+
+  // Load in routes:
+  app.get('/alive', require('./api/alive.js')(db));
+  app.get('/spots', require('./api/get-spots.js')(db));
+  app.post('/spots', require('./api/post-spots.js')(db));
+  app.put('/spots/:id', require('./api/put-spot.js')(db));
+});
 
 // only bootstrapped, not run (for testing)
 module.exports = app;
