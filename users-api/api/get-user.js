22398b63c41a0f94ab110af980990a3147ccf9f9
Angeline Kimbrell <Angeline Kimbrell>
2017-04-01 19:08:53 -0400

    added db param and people variable

--- users-api/api/get-user.js
+++ users-api/api/get-user.js
@@ -1,9 +1,11 @@
 //app/get-user.js
 
+var people = db.collection('users');
+
 // GET /users/:id RequestObject:
 // none, just simple get
 // ResponseObject
-module.exports = function() {
+module.exports = function(db) {
   return function(req, res) {
       people.find({_id: req.params.id}).toArray(function(err, result) {
         if(err) {
