5df00331d15c9021dc5bbc7a6b1184682aceb315
Matthew Siragusa <Matthew Siragusa>
2017-04-03 13:06:48 -0400

    Issue 69 Updated

--- spots-api/api/post-spots.js
+++ spots-api/api/post-spots.js
@@ -2,7 +2,6 @@
 
 module.exports = function(db) {
   return function(req, res) {
-    res.json(req.body);
     var spots = db.collection('spots');
     spots.insert({req.body}, function(err, result) {
       if(err) throw err;
