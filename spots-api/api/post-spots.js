d017d402d89f7d1a2089f73a2eb418c413d82d50
Matt Siragusa <siragusamatt@gmail.com>
2017-04-03 21:51:21 -0400

    Issue 69 made changes

--- spots-api/api/post-spots.js
+++ spots-api/api/post-spots.js
@@ -3,9 +3,16 @@
 module.exports = function(db) {
   return function(req, res) {
     var spots = db.collection('spots');
-    spots.insert({req.body}, function(err, result) {
-      if(err) throw err;
-      res.json(result);
+    spots.insert(req.body, function(err, result) {
+      if(err) {
+        res.status(500).json{(
+          code: "dbError",
+          err: err
+        )};
+      } else {
+        console.log("Created Spot: ", result);
+        res.json(result);
+      }
     });
   }
 };
