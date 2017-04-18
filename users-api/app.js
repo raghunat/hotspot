f31ebb82bc7f33c8edeaf126e3aa25ed02234c2e
Stephen Raghunath <straghunath@gmail.com>
2017-04-18 06:13:47 -0400

    Update app.js

--- users-api/app.js
+++ users-api/app.js
@@ -1,11 +1,5 @@
 // app.js
 // Bootstrapping the express application
-
-// Loads all routes
-
-
-
-
 const app = require('express')();
 const cors = require('cors');
 const bodyParser = require('body-parser');
