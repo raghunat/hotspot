beca38ae0192cc8eac7c63913ca308052f83e702
Matt Siragusa <siragusamatt@gmail.com>
2017-04-19 19:06:47 -0400

    Changed nearby-spots syntax and added comments

--- gateway-api/api/nearby-spots.js
+++ gateway-api/api/nearby-spots.js
@@ -6,26 +6,36 @@ module.exports = function(db) {
 
     superagent
     .get('http://localhost:3000/spots')
-    if(err) {
-      res.status(500).json({
-        code: 'DbError',
-        err: err
-      });
-    } else {
-        res.json(spots);
-        var spotsToReturn = [];
-        for(var i = 0; i < spots.length; i++) {
-          if (res.body.location[0] > spots[i].location[0][0] &&
-            res.body.location[0] < spots[i].location[1][0] &&
-            res.body.location[1] > spots[i].location[0][0] &&
-            res.body.location[1] < spots[i].location[3][0])
-            {
-              spotsToReturn.push(spots[i]);
-            } else {
-              res.send('No nearby spots.');
-            }
-        }
-        res.json(spotsToReturn);
-    }
+    .end((err, response) => {
+      if(err) {
+        res.status(500).json({
+          code: 'DbError',
+          err: err
+        });
+      } else {
+          res.json(spots);
+          var spotsToReturn = [];
+          for(var i = 0; i < spots.length; i++) {
+            // res.body.location is the array the latitude and longitude from the user
+            // spots[i].location is the array from the spots object
+
+              //if latitude is greater than bottom left location
+            if (res.body.location[0] > spots[i].location[0][0] &&
+                //if latitude is less than top left location
+              res.body.location[0] < spots[i].location[1][0] &&
+                //if longitude is grater than bottom left loaction
+              res.body.location[1] > spots[i].location[0][0] &&
+                //if longitude is less than bottom right location
+              res.body.location[1] < spots[i].location[3][0])
+              {
+                spotsToReturn.push(spots[i]);
+              } else {
+                res.send('No nearby spots.');
+              }
+          }
+          res.json(spotsToReturn);
+      }
+    });
+
   }
 };
