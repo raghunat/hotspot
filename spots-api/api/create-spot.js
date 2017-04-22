863d33746032dc2e93b119a67c7d74590eb5cf9b
Stephen Raghunath <straghunath@gmail.com>
2017-04-22 08:00:03 -0400

    Update create-spot.js

--- spots-api/api/create-spot.js
+++ spots-api/api/create-spot.js
@@ -5,20 +5,15 @@ module.exports = function(db) {
     var superagent = require('superagent');
 
     superagent
-    .post('http://localhost:3000/new-spot/')
-    .send({
-    long: '123.456',
-      lat: '456.123'
-      radius: '30'
-    })
-     .set('Accept', 'application/json')
-   .end((err, agentRes)=> {
-     if(err){
-        return(res.status(500).json(err))
-      }
-     else {
-        return res.json({SpotObject})
-      }
-    })
+      .post('http://localhost:3000/spots/')
+      .send(req.body)
+      .end((err, agentRes)=> {
+         if(err){
+            return res.status(500).json(err);
+          }
+         else {
+            return res.json(agentRes.body);
+          }
+      });
   }
 }
