017734b273c52b795f6316ad0dfd43a735030cef
Stephen Raghunath <straghunath@gmail.com>
2017-04-22 07:56:16 -0400

    Update alive.js

--- spots-api/api/alive.js
+++ spots-api/api/alive.js
@@ -1,11 +1,6 @@
 // api/alive.js
-<<<<<<< HEAD
-module.exports = function() {
-
-=======
 
 module.exports = function(db) {
->>>>>>> 998dc4cd00c296f707e9d615857d9ece4318cb06
   // Returns an express req/res callback function
   return function(req, res) {
     db.listCollections().toArray((err, collections) => {
