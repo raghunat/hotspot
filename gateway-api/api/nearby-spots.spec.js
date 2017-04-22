91498782b9c8b2a6d75cd29d08523f0d2c55709a
Stephen Raghunath <straghunath@gmail.com>
2017-04-22 08:02:11 -0400

    Update nearby-spots.spec.js

--- gateway-api/api/nearby-spots.spec.js
+++ gateway-api/api/nearby-spots.spec.js
@@ -5,7 +5,10 @@ const should = require('should');
 describe('Testing if a spot is nearby', () => {
   it('should check to see if any spots are nearby ', (done) => {
     supertest(app)
-      .get('/nearby-spots')
+      .post('/nearby-spots')
+      .send({
+        location: [1, 2]
+      })
       .expect(200)
       .end((err, res) => {
         res.body.should.be.an.Array;
