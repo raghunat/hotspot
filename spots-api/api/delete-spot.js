4eb3b458dd8d2fd4d7117a8caaf1bd98e9def8d0
Stephen Raghunath <straghunath@gmail.com>
2017-04-22 07:58:53 -0400

    Update delete-spot.js

--- spots-api/api/delete-spot.js
+++ spots-api/api/delete-spot.js
@@ -1,20 +1,11 @@
 //delete-spot.js
 
-<<<<<<< HEAD
-
-
 module.exports = function (db) {
   return function (req, res) {
     var spots = db.collection('spots');
-  spots.remove(  spots.find ({_id: req.params.id}), {$set: req.body}, false, function(err, result) {
-
+    spots.remove({_id: req.params.id}, function(err, result) {
       if(err) throw err;
-
-  res.json('Spot deleted');
-=======
-module.exports = function () {
-  return function (req, res) {
-    res.send('spot deleted');
->>>>>>> 998dc4cd00c296f707e9d615857d9ece4318cb06
+      res.send('Spot deleted');
+    });
   }
 }
